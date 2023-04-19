f03c748a27639e82b9cc8698336ecaf291624223
Asuka <yalingliu8888@gmail.com>
2023-04-18 22:11:28 -0700

    use weatherbit api

--- server/index.js
+++ server/index.js
@@ -10,23 +10,23 @@ app.get('/weather/:city', async (req, res) => {
     const city = req.params.city;
   
     try {
-      const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+      const response = await axios.get(`https://api.weatherbit.io/v2.0/current?city=${city}&key=${API_KEY}&units=M`);
       const weatherData = {
-        temperature: response.data.main.temp,
-        location: response.data.name,
-        date: new Date(response.data.dt * 1000).toLocaleString(),
-        sunset: new Date(response.data.sys.sunset * 1000).toLocaleTimeString(),
-        humidity: response.data.main.humidity,
-        pressure: response.data.main.pressure,
-        windSpeed: response.data.wind.speed,
-        icon: response.data.weather[0].icon,
+        temperature: response.data.data[0].temp,
+        location: response.data.data[0].city_name,
+        date: new Date(response.data.data[0].ts * 1000).toLocaleString(),
+        sunset: new Date(response.data.data[0].sunset * 1000).toLocaleTimeString(),
+        humidity: response.data.data[0].rh,
+        pressure: response.data.data[0].pres,
+        windSpeed: response.data.data[0].wind_spd,
+        icon: response.data.data[0].weather.icon,
       };
   
       res.json(weatherData);
     } catch (error) {
       res.status(500).json({ message: 'Error fetching weather data', error: error.message });
     }
-  });
+  });  
   
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
